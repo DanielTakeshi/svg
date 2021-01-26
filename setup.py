c087be190abe7a1a5a4f876e73e213d83ae05175
DanielTakeshi <takeshidanny@gmail.com>
2021-01-26 12:06:45 -0500

    remove the gym cloth one :)

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup
-from setuptools.extension import Extension
-
-ext_modules = [
-    Extension('gym_cloth.physics.cloth',   ['gym_cloth/physics/cloth.pyx']   ),
-    Extension('gym_cloth.physics.gripper', ['gym_cloth/physics/gripper.pyx'] ),
-    Extension('gym_cloth.physics.point',   ['gym_cloth/physics/point.pyx']   ),
-]
 
 setup(
     name='svg',
